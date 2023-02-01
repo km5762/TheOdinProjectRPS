8931d869baaac6d15708b24fbbcaadcc9b882a8d
km5762 <km5762@DESKTOP-587P2TU>
2023-02-01 17:32:52 -0500

    Initial commit

---
+++ scripts.js
@@ -0,0 +1,55 @@
+function getComputerChoice() {
+    let computerMove = Math.floor(Math.random() * 2);
+
+    if (computerMove = 0) {
+        return "Rock"
+    } else if (computerMove = 1) {
+        return "Paper"
+    } else
+        return "Scissors"
+}
+
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase()
+    computerSelection = computerSelection.toLowerCase()
+
+    if (playerSelection === computerSelection) {
+        console.log("Tie!")
+        return "tie";
+    } else if (playerSelection === "paper" && computerSelection === "scissors" || 
+        playerSelection === "rock" && computerSelection === "paper" ||
+        playerSelection === "scissors" && computerSelection === "rock") {
+        console.log(`You lose! ${computerSelection} beats ${playerSelection}`)
+        return "loss"
+    } else {
+        console.log(`You win! ${playerSelection} beats ${computerSelection}`)
+        return "win"
+    }
+}
+
+function game() {
+    let computer = 0
+    let user = 0
+
+    for (let i = 0; i < 5; i++) {
+        if (computer < 3 && user < 3) {
+            let playerSelection = prompt("What is your move?")
+            let computerSelection = getComputerChoice();
+            let result = playRound(playerSelection, computerSelection)
+            if (result === "win") {
+                user++
+            } else if (result == "loss") {
+                computer++
+            }
+        } else {
+            if (computer == 3) {
+                console.log("Computer wins!")
+            } else {
+                console.log("You win!")
+            }
+            break
+        }
+    }
+}
+
+game()
